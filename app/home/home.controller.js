14443704213974d0efa3d90e1ceaf0fd7898c6d3
Warren Wolff <ww.warren.wolff@gmail.com>
2017-09-15 02:01:32 +0200

    Section done!

--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,14 +1,17 @@
 angular.module('myApp.Home', [])
 .controller('HomeCtrl', ['$scope', 'getSection', function($scope, getSection) {
     var vm = this;
-  
-    vm.hello ="aaaah";
     
     vm.getSection = function(stringNumber){
+      vm.sectionName = stringNumber;
       vm.section = getSection.get(stringNumber).then(function(response){
         vm.section = response.data.item;
       });
     }
   
+    vm.match = function(val1, val2) {
+      return val1 == val2;
+    }
+    
     vm.getSection('one');
   }])
