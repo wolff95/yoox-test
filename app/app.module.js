b1e3ca4046802b0d0c69402370ca8b9875fdd115
Warren Wolff <ww.warren.wolff@gmail.com>
2017-09-18 02:00:49 +0200

    carousel almost done!

--- app/app.module.js
+++ app/app.module.js
@@ -4,6 +4,7 @@
 angular.module('myApp', [
   'ngRoute',
   'ngAnimate',
+  'dang-jssor',
   'myApp.Main',
   'myApp.Home'
 ])
--- app/home/home.controller.js
+++ app/home/home.controller.js
@@ -1,8 +1,8 @@
 angular.module('myApp.Home', [])
-.controller('HomeCtrl', ['$scope', 'getSection',  function($scope, getSection) {
+.controller('HomeCtrl', ['$scope', 'getSection', function($scope, getSection) {
     var vm = this;
 
-    vm.open = true;
+    vm.open = false;
 
     vm.getSection = function(stringNumber){
       vm.sectionName = stringNumber;
@@ -14,6 +14,52 @@ angular.module('myApp.Home', [])
     vm.match = function(val1, val2) {
       return val1 == val2;
     }
+
+    jssor_1_slider_init = function () {
+     /* 
+        var jssor_1_options = {
+          $AutoPlay: 1,
+          $SlideWidth: 720,
+          $Cols: 2,
+          $Align: 130,
+          $ArrowNavigatorOptions: {
+            $Class: $JssorArrowNavigator$
+          },
+          $BulletNavigatorOptions: {
+            $Class: $JssorBulletNavigator$
+          }
+        };*/
+
+        var jssor_1_slider = new $JssorSlider$("jssor_1", jssor_1_options);
+         
+        /*#region responsive code begin*/
+
+        var MAX_WIDTH = 980;
+
+        function ScaleSlider() {
+          var containerElement = jssor_1_slider.$Elmt.parentNode;
+          var containerWidth = containerElement.clientWidth;
+
+          if (containerWidth) {
+
+            var expectedWidth = Math.min(MAX_WIDTH || containerWidth, containerWidth);
+
+            jssor_1_slider.$ScaleWidth(expectedWidth);
+          }
+          else {
+            window.setTimeout(ScaleSlider, 30);
+          }
+        }
+
+        ScaleSlider();
+
+        $Jssor$.$AddEvent(window, "load", ScaleSlider);
+        $Jssor$.$AddEvent(window, "resize", ScaleSlider);
+        $Jssor$.$AddEvent(window, "orientationchange", ScaleSlider);
+        /*#endregion responsive code end*/
+    };
+
+    
 
     vm.getSection('one');
   }])
